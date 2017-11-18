7e8c1d829d289a663abbc72ebb10f13455451609
Balhau <balhau@balhau.net>
2017-11-18 13:10:00 +0000

    Downgraded the version of ghost

--- apps/codecorner/scripts/installghost.sh
+++ apps/codecorner/scripts/installghost.sh
@@ -1,5 +1,5 @@
-wget https://ghost.org/zip/ghost-latest.zip --no-check-certificate
-unzip -d /opt/ghost ghost-latest.zip
+wget https://github.com/TryGhost/Ghost/releases/download/0.11.8/Ghost-0.11.8.zip --no-check-certificate
+unzip -d /opt/ghost Ghost-0.11.8.zip
 cd /opt/ghost
 npm install --production
 touch /opt/ghost/install.lock
