55fc403fa0e3b237acec9fc92d9b7333cd0d0e98
Balhau <balhau@balhau.net>
2017-11-18 13:45:04 +0000

    Refactor on codecorner ghost instance container

--- apps/codecorner/scripts/codecorner.sh
+++ apps/codecorner/scripts/codecorner.sh
@@ -1,4 +1,4 @@
 [ -f /opt/ghost/install.lock ] && echo "Installation exists skipping setup" || (echo "Instataling ghost blog" && /opt/scripts/installghost.sh)
 echo "Initializing knex-migrator"
-cd /opt/ghost && knex-migrator init --mgpath /opt/ghost/MigratorConfig.js
+//cd /opt/ghost && knex-migrator init --mgpath /opt/ghost/MigratorConfig.js
 cd /opt/ghost && npm start
