a3b39a18997177a7af396ca52c95a67cfc41d436
Balhau <balhau@balhau.net>
2017-11-19 16:59:42 +0000

    More on php and gitlist containers

--- apps/gitlist/scripts/gitlist-install.sh
+++
@@ -1,3 +0,0 @@
-cd /tmp && curl --silent --ssl https://s3.amazonaws.com/gitlist/gitlist-0.5.0.tar.gz | tar zxf -
-cp -R /tmp/gitlist/* /www/ && rm -rf /tmp/gitlist
-touch /www/install.lock
--- apps/gitlist/scripts/gitlist.sh
+++ apps/gitlist/scripts/gitlist.sh
@@ -1,2 +1,9 @@
-[ -f /www/install.lock ] && echo "Installation exists skipping setup" || (echo "Instataling gitlist blog" && /opt/scripts/gitlist-install.sh)
-echo "Initializing knex-migrator"
+if [ -f /app/install.lock ] 
+then
+    echo "Installation exists skipping setup" 
+else
+    echo "Instataling gitlist blog on /app folder"
+    cd /tmp && curl --silent --ssl https://s3.amazonaws.com/gitlist/gitlist-0.5.0.tar.gz | tar zxf -
+    cp -R /tmp/gitlist/* /app/ && rm -rf /tmp/gitlist
+    touch /app/install.lock
+fi
